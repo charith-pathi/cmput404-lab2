5b53a456bf7d21ccbf3b39262c889c62bc9b3d0b
charith-pathi <c_pathirathna@hotmail.com>
2023-01-18 18:03:40 -0700

    Added echo_server, proxy_client and proxy_server files

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ BYTES_TO_READ = 4096
 
 def get(host, port):
 	# create request
-	request_data = b"GET / HTTP/1.1\nHost:" + host.encode('utf-8') + '\n\n'
+	request_data = b"GET / HTTP/1.1\nHost:" + host.encode('utf-8') + "\n\n"
 
 	# create socket
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,3 +20,5 @@ def get(host, port):
 		response = s.recv(BYTES_TO_READ)
 
 	s.close()
+
+get("www.google.com", 80)
