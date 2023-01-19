c76f12d03e33fab368bcf0d53a567ea73c8887a2
charith-pathi <c_pathirathna@hotmail.com>
2023-01-18 17:23:46 -0700

    Initial client.py setup

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+BYTES_TO_READ = 4096
+
+def get(host, port):
+	# create request
+	request_data = b"GET / HTTP/1.1\nHost:" + host.encode('utf-8') + '\n\n'
+
+	# create socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# send data
+	s.connect((host, port))
+	s.send(request_data)
+	s.shutdown(socket.SHUT_WR)
+
+	response = s.recv(BYTES_TO_READ)
+	while(len(response) > 0):
+		print(response)
+		response = s.recv(BYTES_TO_READ)
+
+	s.close()
